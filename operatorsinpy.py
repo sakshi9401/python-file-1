57195f56ac3ae4ac7d4a9bafe17dddb7eda11228
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-08-07 12:58:45 +0530

    Add files via upload

---
+++ operatorsinpy.py
@@ -0,0 +1,22 @@
+#operators in python
+#1)arithmatic operator
+print("4+5","is", 4+5)
+print("4-5","is", 4-5)
+print("4*5","is", 4*5)
+print("4/5","is", 4/5)
+print("4%5","is", 4%5)
+print("4//5","is", 4//5)
+print("2**5","is", 2**5)
+#2)comparision operator
+x=5
+x+=2
+print(x)
+y=4
+y-=1
+print(y)
+z=9
+z*=2
+print(z)
+a=6
+a/=2
+print(a)
